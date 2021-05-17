a461b5ce7d16f96f41acbe73a46e6944ce28f801
murad44 <murad_amanov186@icloud.com>
2021-05-17 13:00:49 +0400

    Add project files.

---
+++ MyProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transport car = new Transport("BMW", "M5", 50, 80, 15);
+            string input;
+            do
+            {
+                Console.WriteLine("Please, enter a number:");
+                Console.WriteLine("1. Go");
+                Console.WriteLine("2. TopUp");
+                Console.WriteLine("3. Stop");
+                Console.WriteLine("4. Exit");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        car.Go();
+                        break;
+                    case "2":
+                        car.TopUp();
+                        break;
+                    case "3":
+                        Console.WriteLine("You have stopped the car.");
+                        break;
+                    case "4":
+                        Console.WriteLine("GoodBye!");
+                        break;
+                    default:
+                        Console.WriteLine("Please, enter the number from the list.");
+                        break;
+                }
+            }
+            while (input != "4");
+        }
+    }
+}
---
+++ MyProject/Transport.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject
+{
+    class Transport
+    {
+        public string Mark;
+        public string Model;
+        public decimal CurrentFuel;
+        public decimal MaxCapacity;
+        public decimal FuelEff;
+
+        public Transport(string mark, string model, decimal currentFuel, decimal maxCapacity, decimal fuelEff)
+        {
+            Mark = mark;
+            Model = model;
+            CurrentFuel = currentFuel;
+            MaxCapacity = maxCapacity;
+            FuelEff = fuelEff;
+        }
+
+        public void Go()
+        {
+        Back:
+            Console.WriteLine("Please, enter the distance length:");
+            string neededLength = Console.ReadLine();
+
+            try
+            {
+                decimal neededLengthD = Convert.ToDecimal(neededLength);
+                if (CurrentFuel >= neededLengthD * FuelEff / 100)
+                {
+                    CurrentFuel -= neededLengthD * FuelEff / 100;
+                    Console.WriteLine($"You pave passed {neededLengthD} km. There are {CurrentFuel} litres left.");
+                }
+                else
+                {
+                    Console.WriteLine("There is not enough fuel in tank. Please, fill it.");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please, enter a numeric number.");
+                goto Back;
+            }
+        }
+        public void TopUp()
+        {
+            decimal neededFuelD = 0;
+            while (neededFuelD == 0)
+            {
+                Console.WriteLine("How much litres do yo want to add?");
+                string neededFuelEff = Console.ReadLine();
+                try
+                {
+                    neededFuelD = Convert.ToDecimal(neededFuelEff);
+                    if(MaxCapacity >= neededFuelD + CurrentFuel)
+                    {
+                        CurrentFuel += neededFuelD;
+                        Console.WriteLine($"You have added {neededFuelD} litres. There are {CurrentFuel} litres in tank.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The tank capacity isn't enough!");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please, enter a numeric number");
+                }
+            }
+        }
+    }
+}
